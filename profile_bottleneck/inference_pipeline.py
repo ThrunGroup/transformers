941e1736057eeef8a56f5ec982b934842188f94f
vxbrandon <vxbrandon@gmail.com>
2023-02-21 03:18:34 +0000

    Preliminary implementation of inference pipeline.

---
+++ profile_bottleneck/inference_pipeline.py
@@ -0,0 +1,44 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+import time
+
+from typing import List
+
+
+def inference_pipeline(model_names: List[str] = ["bert-base-cased"], num_experiments: int = 1):
+    # Set up the tokenizer and models
+    models = {}
+    for model_name in model_names:
+        models[model_name] = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+    # Set up the pipeline for sentiment analysis
+    sentiment_analysis_pipelines = {
+        model_name: pipeline("sentiment-analysis", model=model, tokenizer=AutoTokenizer.from_pretrained(model_name))
+        for model_name, model in models.items()
+    }
+
+    # Set up sequence lengths to test
+    sequence_lengths = [16, 32, 64, 128, 256, 512]
+
+    # Benchmark the inference time for each model and sequence length
+    for model_name, sentiment_analysis_pipeline in sentiment_analysis_pipelines.items():
+        print(f"Model: {model_name}")
+        for sequence_length in sequence_lengths:
+            input_text = "I'm 3 years older than my brother. My brother is 2 years old. What is my age" * (
+                sequence_length // 6
+            )
+            inference_time = 0
+
+            for idx in range(num_experiments):
+                start_time = time.time()
+                output = sentiment_analysis_pipeline(input_text)
+                end_time = time.time()
+                inference_time += end_time - start_time
+            inference_time /= num_experiments
+            print(f"Sequence length: {sequence_length}, Inference time: {inference_time:.4f} seconds")
+
+
+if __name__ == "__main__":
+    inference_pipeline(model_names=["google/bigbird-roberta-base"])
+
+
+
