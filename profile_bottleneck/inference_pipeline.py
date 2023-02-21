21f74e4e4afd95aede63c037b94d5370e65abcc2
vxbrandon <vxbrandon@gmail.com>
2023-02-21 20:00:09 +0000

    Preliminary implementation of inference pipeline.

--- profile_bottleneck/inference_pipeline.py
+++ profile_bottleneck/inference_pipeline.py
@@ -1,4 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers.pipelines.pt_utils import KeyDataset
+from datasets import load_dataset
+
 import time
 
 from typing import List
@@ -11,17 +14,27 @@ def inference_pipeline(model_names: List[str] = ["bert-base-cased"], num_experim
         models[model_name] = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
     # Set up the pipeline for sentiment analysis
-    sentiment_analysis_pipelines = {
-        model_name: pipeline("sentiment-analysis", model=model, tokenizer=AutoTokenizer.from_pretrained(model_name))
+    text_classification_pipelines = {
+        model_name: pipeline("text-classification", model=model, tokenizer=AutoTokenizer.from_pretrained(model_name))
         for model_name, model in models.items()
     }
 
     # Set up sequence lengths to test
-    sequence_lengths = [16, 32, 64, 128, 256, 512]
-
+    # sequence_lengths = [512, 1024]
+    # dataset = load_dataset('glue', 'cola', split='test')
+    # print(f"Number of test samples is {len(dataset)}")
+    # dataset = dataset['sentence']
+    sequence_lengths = [500, 1000]
     # Benchmark the inference time for each model and sequence length
-    for model_name, sentiment_analysis_pipeline in sentiment_analysis_pipelines.items():
-        print(f"Model: {model_name}")
+    for model_name, text_classification_pipeline in text_classification_pipelines.items():
+        # start_time = time.time()
+        # output = text_classification_pipeline(dataset)
+        # end_time = time.time()
+        # inference_time = end_time - start_time
+        # # print(output)
+        # print(f" Inference time: {inference_time:.4f} seconds")
+        #
+        # print(f"Model: {model_name}")
         for sequence_length in sequence_lengths:
             input_text = "I'm 3 years older than my brother. My brother is 2 years old. What is my age" * (
                 sequence_length // 6
@@ -30,7 +43,7 @@ def inference_pipeline(model_names: List[str] = ["bert-base-cased"], num_experim
 
             for idx in range(num_experiments):
                 start_time = time.time()
-                output = sentiment_analysis_pipeline(input_text)
+                output = text_classification_pipeline(input_text)
                 end_time = time.time()
                 inference_time += end_time - start_time
             inference_time /= num_experiments
